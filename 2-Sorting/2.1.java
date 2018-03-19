326bbda47f76f8e1bdb5823c4e83cfac0bdbea64
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-18 23:57:01 -0700

    Writing out the example class for sorting

--- 2-Sorting/2.1.java
+++ 2-Sorting/2.1.java
@@ -1,1 +1,7 @@
 // Template for sort class
+public class Example {
+  public static void sort(Comparable[] a) {}
+  private static boolean less(Comparable v, Comparable w)
+  { return v.compareTo(w) < 0; }
+  private 
+}
