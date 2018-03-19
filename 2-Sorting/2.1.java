efea889251e16802ec1cdf0b17aebfbb9b9d23f8
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-18 23:59:09 -0700

    added exchange method for arrays in the sorting example class

--- 2-Sorting/2.1.java
+++ 2-Sorting/2.1.java
@@ -3,5 +3,6 @@ public class Example {
   public static void sort(Comparable[] a) {}
   private static boolean less(Comparable v, Comparable w)
   { return v.compareTo(w) < 0; }
-  private 
+  private static void exchange(Comparable[] a, int i, int j)
+  { Comparable t = a[i]; a[i] = a[j]; a[j] = t; }
 }
