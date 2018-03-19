86f38780886e33e9983124a803606487752be062
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-19 00:03:05 -0700

    show method added for sorting example class

--- 2-Sorting/2.1.java
+++ 2-Sorting/2.1.java
@@ -1,8 +1,19 @@
 // Template for sort class
 public class Example {
   public static void sort(Comparable[] a) {}
+
   private static boolean less(Comparable v, Comparable w)
   { return v.compareTo(w) < 0; }
+
   private static void exchange(Comparable[] a, int i, int j)
   { Comparable t = a[i]; a[i] = a[j]; a[j] = t; }
+
+  private static void show(Comparable[] a) {
+    for (int i = 0; i < a.length; i++) {
+      StdOut.print(a[i] + " ");
+    }
+    StdOut.println();
+  }
+
+  
 }
