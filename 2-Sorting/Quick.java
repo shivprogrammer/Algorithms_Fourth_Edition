c03177ad75b12250a921ca39e4cb827d6e627fec
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-20 22:51:58 -0700

    adding in Comparable subarray with highest and lowest elements given as parameters of function

--- 2-Sorting/Quick.java
+++ 2-Sorting/Quick.java
@@ -12,4 +12,13 @@ public class Quick {
     sort(a, 0, a.length - 1);
     assert isSorted(a);
   }
+
+  // quicksort the subarray from a[lo] to a[hi]
+  private static void sort(Comparable[] a, int lo, int hi) {
+    if (hi <= lo) return;
+    int j = partitiion(a, lo, hi);
+    sort(a, lo, j - 1);
+    sort(a, j + 1, hi);
+    assert isSorted(a, lo, hi);
+  }
 }
