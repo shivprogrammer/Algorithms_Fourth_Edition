ff0498305d3044fa5deb369c685429a1ee043d15
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-25 00:27:07 -0700

    Finished off DeDup (remove duplicates) class in java

---
+++ Searching/DeDup.java
@@ -0,0 +1,17 @@
+public class DeDup {
+  // Do not instantiate.
+  private DeDup() { }
+
+  public static void main(String[] args) {
+    SET<String> set = new SET<String>();
+
+    // read in strings and add to set
+    while (!StdIn.isEmpty()) {
+      String key = StdIn.readString();
+      if (!set.contains(key)) {
+        set.add(key);
+        StdOut.println(key);
+      }
+    }
+  }
+}
