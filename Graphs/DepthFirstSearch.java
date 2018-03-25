fe030feb15fda92a5d1357e4fff544b82b832482
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-25 00:13:27 -0700

    Finished off DepthFirstSearch

--- Graphs/DepthFirstSearch.java
+++ Graphs/DepthFirstSearch.java
@@ -27,5 +27,22 @@ public class DepthFirstSearch {
     return count;
   }
 
-  
+  private void validateVertex(int v) {
+    int V = marked.length;
+    if (v < 0 || v >= V)
+      throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+  }
+
+  public static void main(String[] args) {
+    In in = new In(args[0]);
+    Graph G = new Graph(in);
+    DepthFirstSearch search = new DepthFirstSearch(G, s);
+    for (int v = 0; v < G.V(); v++) {
+      if (search.marked(v))
+        StdOut.print(v + " ");
+    }
+    StdOut.println();
+    if (search.count() != G.V()) StdOut.println("NOT connected");
+    else StdOut.println("connected");
+  }
 }
