e51f189ffd7d9a495b58e1f49f92afff3de20d07
shivprogrammer <shiv.unadkat@gmail.com>
2018-03-30 21:54:21 -0700

    building out DepthFirstPaths

--- Graphs/DepthFirstPaths.java
+++ Graphs/DepthFirstPaths.java
@@ -1,1 +1,18 @@
 // The DepthFirstPaths class represents a data type for finding paths from a source vertex s to ever other vertex in an undirected graph.
+
+public class DepthFirstPaths {
+  private boolean[] marked;
+  private int[] edgeTo;
+  private final int s;
+
+  // Computes a path between s and every other vertex in the graph
+  public DepthFirstPaths(Graph G, int s) {
+    this.s = s;
+    edgeTo = new int[G.V()];
+    marked = new boolean[G.V()];
+    validateVertex(s);
+    dfs(G, s);
+  }
+
+  //
+}
